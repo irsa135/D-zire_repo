af497113eb271e4c5de986429799a2e6a69b379b
lenovo <lenovo@DESKTOP-Q5444MP>
2022-11-29 20:50:18 +0500

    Add project files.

---
+++ homework/Controllers/Home.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace homework.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult Sizechart()
+        {
+            return View();
+        }
+    }
+}
